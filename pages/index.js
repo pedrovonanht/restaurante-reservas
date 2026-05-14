8bed56baa00eb1aa810e63f2681de8a119281bd1
Pedro Von Anht <123915039+pedrovonanht@users.noreply.github.com>
2026-05-14 19:46:57 +0000

    install tailwindcss

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,7 @@
-export default function HelloWorld () {
-    return(<><h1>Olá mundo, olá breno</h1></>)
+export default function HelloWorld() {
+  return (
+    <>
+      <h1 className="text-amber-300">Olá mundo, olá breno</h1>
+    </>
+  );
 }
