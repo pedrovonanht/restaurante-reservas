d822d78e127f7fbbc7cd243d3462d3684a29c13d
Pedro Von Anht <123915039+pedrovonanht@users.noreply.github.com>
2026-05-15 19:35:17 +0000

    chore: small file name changes and eslint fixes + gitignore .swc add

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,7 @@
-export default {
+const configs = {
   plugins: {
     '@tailwindcss/postcss': {},
   },
 }
+
+export default configs;
