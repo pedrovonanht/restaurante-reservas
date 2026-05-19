7fce9b232cc2556678ae0a1e22a199839691bf44
Pedro Von Anht <123915039+pedrovonanht@users.noreply.github.com>
2026-05-19 23:26:43 +0000

    feat: add route `api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,3 +1,21 @@
-export default function status(request, response) {
-    return response.status(200).json({})
+import database from "@/infra/database.js";
+
+export default async function status(request, response) {
+  const result = await database.query({
+    text: `
+      SELECT
+        current_setting('server_version') AS version,
+        current_setting('max_connections')::int AS max_conections,
+        (SELECT count(*)::int FROM pg_stat_activity WHERE datname = current_database()) AS used_conections
+    `,
+  });
+
+  const { version, max_conections, used_conections } = result.rows[0];
+
+  return response.status(200).json({
+    version,
+    max_conections,
+    used_conections,
+    updated_at: new Date().toISOString(),
+  });
 }
