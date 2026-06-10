ee07dbbb04da5425fa0dd84c9ffb3676e4d76420
Pedro Von Anht <123915039+pedrovonanht@users.noreply.github.com>
2026-06-10 20:12:31 +0000

    build: makes `npm run dev` run all services before run

---
+++ infra/scripts/wait-for-postgres.js
@@ -0,0 +1,16 @@
+import { exec } from 'node:child_process'; //módulo do node que permite rodar scripts no código
+
+function verifyPostgressConection () {
+  exec("docker exec postgress-dev pg_isready --host localhost", HandlerReturn) //script + callback //postgress-dev é o nome do container e deve ser mudado no arquivo yaml
+	//-- host especifica que conexão verificada é TCP/IP não Socket Unix
+  function HandlerReturn (error, stdout) { //stdout = standart output
+    if (stdout.search("accepting connections") === -1) { //método .search para procurar string
+      process.stdout.write(".") //escrevendo no console desse jeito não há quebra de linha
+      verifyPostgressConection() //ao invés de while ou for usamos recursividade 
+      return
+    }
+    console.log("\n 🟢 Conexão aberta e pronta para receber conexões! \n")
+  }
+}
+process.stdout.write("🔴 aguardando conexão com postgress...")
+verifyPostgressConection();
