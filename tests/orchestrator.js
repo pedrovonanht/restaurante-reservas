288306b8990fc93d736d9ed4fef4c3e954574062
Pedro Von Anht <123915039+pedrovonanht@users.noreply.github.com>
2026-06-10 20:47:22 +0000

    build: estabilize `npm run test` script

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -1,11 +1,28 @@
 import database from "infra/database";
+import retry from "async-retry";
+import webserver from "infra/webserver";
 
 async function clearDatabase () {
     await database.query("DROP SCHEMA PUBLIC CASCADE; CREATE SCHEMA PUBLIC;")
 } 
 
+async function waitForAllServices() {
+     return retry(fetchStatusPage, {
+      retries: 100,
+      maxTimeout: 1000,
+    });
+
+     async function fetchStatusPage() {
+      const response = await fetch(`${webserver.origin}/api/v1/status`);
+
+      if (response.status !== 200) {
+        throw Error();
+      }
+}}
+
 const orchestrator = {
-    clearDatabase
+    clearDatabase,
+    waitForAllServices
 }
 
 export default orchestrator;
